0e04ec934c47d9a558d2fd394ea55acfe17ef2fc
Penny Chase <mpchase@mac.com>
2022-11-29 16:41:54 -0500

    Chapter 1 greeting example

--- chapter1/Main.hs
+++ chapter1/Main.hs
@@ -1,3 +1,35 @@
 module Main where
 
-main = print "Hello World"
+makeGreeting salutation person =
+    salutation <> " " <> person
+
+greetPerson = makeGreeting "Hello"
+
+enthusiasticGreeting salutation = makeGreeting (salutation <> "!")
+
+greetGeorge = (`makeGreeting` "George")
+
+greetGeorge' = flip makeGreeting "George"
+
+-- pointfree version of makeGreeting
+
+-- First refactor
+makeGreeting' = (<>) . firstPart
+    where
+        firstPart salutation = salutation <> " "
+
+-- Full pointfree, substituing defintion of firstPart into code
+makeGreeting'' = (<>) . (<> " ")
+
+extendedGreeting person =
+    helloAndGoodbye "Hello" "Goodbye"
+    where
+        helloAndGoodbye hello goodbye =
+            let hello' = makeGreeting hello person
+                goodbye' = makeGreeting goodbye person
+            in joinWithNewlines hello' goodbye'
+        joinWithNewlines a b = a <> "\n" <> b
+
+main = print "No salutations yet"
+
+
