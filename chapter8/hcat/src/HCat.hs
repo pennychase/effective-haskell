06b2f9b633f2e98d5f0da3307d252ffae460eddb
Penny Chase <mpchase@mac.com>
2022-12-31 16:39:01 -0500

    hcat (chapter 8) - initial command line handling

--- chapter8/hcat/src/HCat.hs
+++ chapter8/hcat/src/HCat.hs
@@ -1,4 +1,24 @@
 module HCat where
 
+import qualified System.Environment as Env
+
+handleArgs :: IO (Either String FilePath)
+handleArgs =
+    parseArgs <$> Env.getArgs
+    where
+        parseArgs argumentList =
+            case argumentList of
+                [fname] -> Right fname
+                []      -> Left "No filename provided"
+                _       -> Left "Multiple files not supported"
+
+ 
+
 runHCat :: IO ()
-runHCat = return ()
+runHCat = 
+    handleArgs >>= displayMessage
+    where
+        displayMessage parsedArgument =
+            case parsedArgument of
+                Left errMessage -> putStrLn $ "Error: " <> errMessage
+                Right filename -> putStrLn $ "Opening file: " <> filename
