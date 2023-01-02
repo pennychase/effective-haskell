905daa68ea92a63bad078157ff8c3a821dcb5420
Penny Chase <mpchase@mac.com>
2023-01-01 19:01:27 -0500

    Chapter 8 - initial version of hcat

--- chapter8/hcat/src/HCat.hs
+++ chapter8/hcat/src/HCat.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module HCat where
 
 import qualified Control.Exception as Exception
+import qualified Data.ByteString as BS
+import qualified Data.Text as Text
+import qualified Data.Text.IO as TextIO
 import qualified System.IO.Error as IOError
 import qualified System.Environment as Env
+import qualified System.Info
+import System.IO 
+import qualified System.Process as Process
 
 
 -- Command line argument processing
@@ -24,16 +31,104 @@ eitherToErr :: Show a => Either a b -> IO b
 eitherToErr (Right a) = return a
 eitherToErr (Left e) = Exception.throwIO . IOError.userError $ show e
 
+--
+-- Paging
+--
+
+-- Group lines of text into pages
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf n [] = []
+groupsOf n elems =
+    let (hd, tl) = splitAt n elems
+    in hd : groupsOf n tl
+
+-- Word wrap 
+wordWrap :: Int -> Text.Text -> [Text.Text]
+wordWrap lineLength lineText
+    | Text.length lineText <= lineLength = [lineText]
+    | otherwise = 
+        let
+            (candidate, nextLines) = Text.splitAt lineLength lineText
+            (firstLine, overflow) = softWrap candidate (Text.length candidate - 1)
+        in firstLine : wordWrap lineLength (overflow <> nextLines)
+    where
+        softWrap hardwrappedText textIndex
+            | textIndex <= 0 = (hardwrappedText, Text.empty)
+            | Text.index hardwrappedText textIndex == ' ' =
+                let (wrappedLine, rest) = Text.splitAt textIndex hardwrappedText
+                in (wrappedLine, Text.tail rest)
+            | otherwise = softWrap hardwrappedText (textIndex - 1)
+
+-- Terminal Dimensions
+data ScreenDimensions = ScreenDimensions
+    { screenRows :: Int
+    , screenColumns :: Int
+    } deriving Show
+
+
+getTerminalSize :: IO ScreenDimensions
+getTerminalSize =
+    case System.Info.os of
+        "darwin" -> tPutScreenDimensions
+        "linux" -> tPutScreenDimensions
+        _other -> pure $ ScreenDimensions 25 80
+    where
+        tPutScreenDimensions :: IO ScreenDimensions
+        tPutScreenDimensions =
+            Process.readProcess "tput" ["lines"] ""
+            >>= \lines ->
+                Process.readProcess "tput" ["cols"] ""
+                >>= \ cols ->
+                    let lines' = read $ init lines
+                        cols' = read $ init cols
+                    in pure $ ScreenDimensions lines' cols'
+
+
+paginate :: ScreenDimensions -> Text.Text -> [Text.Text]
+paginate (ScreenDimensions rows columns) text =
+    let unwrappedLines = Text.lines text
+        wrappedLines = concatMap (wordWrap columns) unwrappedLines
+        pageLines = groupsOf rows unwrappedLines
+    in map Text.unlines pageLines
+
+-- Getting User Input
+data ContinueCancel = Continue | Cancel 
+    deriving (Eq, Show)
+
+getContinue :: IO ContinueCancel
+getContinue =
+    hSetBuffering stdin NoBuffering
+    >> hSetEcho stdin False
+    >> hGetChar stdin
+    >>= \case
+        ' ' -> pure Continue
+        'q' -> pure Cancel
+        _ -> getContinue
+
+-- Paging
+
+clearScreen :: IO ()
+clearScreen = BS.putStr "\^[[1J\^[[1;1H"
+
+showPages :: [Text.Text] -> IO ()
+showPages [] = pure ()
+showPages (page:pages) =
+    clearScreen
+    >> TextIO.putStrLn page
+    >> getContinue
+    >>= \case
+        Continue -> showPages pages
+        Cancel -> return ()
+
 runHCat :: IO ()
 runHCat = 
-    handleIOError $
-        handleArgs
-        >>= eitherToErr
-        >>= readFile
-        >>= putStrLn
-    where
-        handleIOError :: IO () -> IO ()
-        handleIOError ioAction =
-            Exception.catch ioAction $ 
-                \e -> print "I ran into an error" >> print @IOError e -- use @IOError instead of writing a handler
+    handleArgs
+    >>= eitherToErr
+    >>= flip openFile ReadMode
+    >>= TextIO.hGetContents
+    >>= \contents ->
+        getTerminalSize >>= \termSize ->
+            let pages = paginate termSize contents
+            in showPages pages
+
 
